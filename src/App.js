d75790d471fc06548312038ea30a99e526076314
genevieveleanne <genevieveleanned25@gmail.com>
2022-01-25 16:42:24 -0800

    Updated App component. Added footer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
+import WeatherSearch from "./WeatherSearch";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <WeatherSearch />
+      </div>
+      <footer>
+        This app was built by{" "}
+        <a
+          href="https://trusting-curie-946ae6.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Genevieve Leanne Dominguez
+        </a>
+        . It is open-sourced on{" "}
+        <a
+          href="https://github.com/genevieveleanne/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
     </div>
   );
 }

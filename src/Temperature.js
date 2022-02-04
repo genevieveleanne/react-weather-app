7697eeb7691c7fe771769e7e2d76cf0926c0bd12
genevieveleanne <genevieveleanned25@gmail.com>
2022-02-03 20:04:01 -0800

    Removed unit conversion and fixed 5-day forecast

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,45 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Temperature(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function celsius() {
-    return ((props.fahrenheit - 32) * 5) / 9;
-  }
-
-  if (unit === "fahrenheit") {
-    return (
-      <span className="Temperature">
-        <span className="temperature">{Math.round(props.fahrenheit)}</span>
-        <span className="units">
-          °F |
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span className="Temperature">
-        <span className="temperature">{Math.round(celsius())}</span>
-        <span className="units">
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>{" "}
-          | °C
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="Temperature">
+      <span className="temperature">{Math.round(props.fahrenheit)}</span>
+      <span className="units">°F</span>
+    </span>
+  );
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
@@ -7,6 +7,19 @@ import "./WeatherForecast.css";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function search() {
+    const apiKey = "1fc48f0961dfe9fce83c65e9089afb69";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -24,18 +37,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay forecastData={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    const apiKey = "1fc48f0961dfe9fce83c65e9089afb69";
-    let latitude = props.coordinates.lat;
-    let longitude = props.coordinates.lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
-
-    axios.get(apiUrl).then(handleResponse);
+    search();
 
     return null;
   }
